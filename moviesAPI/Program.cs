ec6bb6e670607f55aaaca4adab8617537659a36a
veromont <108094081+veromont@users.noreply.github.com>
2023-07-13 22:37:17 +0300

    added home view and controller, removed swagger

--- moviesAPI/Program.cs
+++ moviesAPI/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using moviesAPI.Repositories;
 using moviesAPI.Models.CinemaContext;
-using System.Text.Json;
 using moviesAPI.Interfaces;
 using moviesAPI.Services;
 using moviesAPI.Validators;
@@ -15,54 +13,33 @@ builder.Services.AddDbContext<CinemaContext>(options =>
     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
 
-builder.Services.AddControllers().AddJsonOptions(x =>
-{
-    x.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-});
-
-builder.Services.AddSwaggerGen(d =>
-{
-    d.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Movies API",
-        Version = "v2",
-        Description =
-                "The system provides the ability to transform Excel to JSON(Import) and JSON to Excel(Export).\n\nAccess to the system functionality is provided via API."
-    }
-    );
-});
-
-builder.Services.AddCors(options =>
-{
-
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-});
-
 builder.Services.AddScoped<GenericCinemaRepository>();
 builder.Services.AddScoped<IPdfTransformService, PdfTransformService>();
 builder.Services.AddTransient<EntityValidator>();
-builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddControllersWithViews();
 
 //resolve fonts once
 MyFontResolver.Apply();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
+
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseCors();
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
